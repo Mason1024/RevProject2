eb4418c8e1e89687255ee44a8564bde84cb1178f
Dillon Mason <d14mason@yahoo.com>
2019-10-24 11:27:47 -0400

    Repos

---
+++ RevProject2/src/main/java/org/stuff/repo/UserRepo.java
@@ -0,0 +1,14 @@
+package org.stuff.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.stuff.entities.User;
+
+@Component
+@Repository
+public interface UserRepo extends CrudRepository<User,Integer>{
+
+	User findByUsername(String username);
+	
+}
