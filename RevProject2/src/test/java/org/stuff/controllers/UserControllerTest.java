a27bdeef0c48945ad08ba76c0691192e2b604ee9
Dillon Mason <d14mason@yahoo.com>
2019-10-25 12:53:00 -0400

    added missing test headers

--- RevProject2/src/test/java/org/stuff/controllers/UserControllerTest.java
+++ RevProject2/src/test/java/org/stuff/controllers/UserControllerTest.java
@@ -1,9 +1,19 @@
 package org.stuff.controllers;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ContextConfiguration(classes=org.stuff.app.RevProject2Application.class)
 public class UserControllerTest {
 
 	@Test
